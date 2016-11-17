b58d0bc286b032689c5c2bc8a5cdbc815304e566
cpp-rakesh <cpp.rakesh@gmail.com>
2016-11-18 01:11:36 +1100

    Solution for Number of 1 Bits

---
+++ Algorithms/number_of_1_bits.cpp
@@ -0,0 +1,26 @@
+/*
+  Solution for Number of 1 Bits
+  Author: cpp.rakesh@gmail.com
+  Date: Nov 18th, 2016
+*/
+
+#include <cstdio>
+#include <stdint.h>
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int bits = 0;
+        for (int i = 0; i < 32; ++i)
+            if ((1 << i) & n)
+                ++bits;
+
+        return bits;
+    }
+};
+
+int main() {
+    Solution s;
+    printf("number of on bits == [%d]\n", s.hammingWeight(32));
+    return 0;
+}
