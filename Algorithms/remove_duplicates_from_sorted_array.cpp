c7721c649fd25ccd12edf6da71cb5c67a2568820
cpp-rakesh <cpp.rakesh@gmail.com>
2016-11-18 01:48:51 +1100

    Adding test case

--- Algorithms/remove_duplicates_from_sorted_array.cpp
+++ Algorithms/remove_duplicates_from_sorted_array.cpp
@@ -3,6 +3,10 @@
   Author : cpp.rakesh@gmail.com
   Date: Sep 3rd, 2016
 */
+
+#include <cstdio>
+#include <vector>
+
 class Solution {
 public:
     int removeDuplicates(std::vector<int>& nums) {
@@ -18,3 +22,23 @@ public:
         return i + 1;
     }
 };
+
+int main() {
+    int A[] = {2, 2, 1, 1, 1, 183, 12, 21, 12, 12};
+    std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
+
+    printf("-----------------------------------------------------------\n");
+    for (std::size_t i = 0; i < V.size(); ++i)
+        printf("%d ", V[i]);
+    printf("\n-----------------------------------------------------------\n");    
+    
+    Solution s;
+    const int size = s.removeDuplicates(V);
+
+    printf("-----------------------------------------------------------\n");
+    for (int i = 0; i < size - 1; ++i)
+        printf("%d ", V[i]);
+    printf("\n-----------------------------------------------------------\n");    
+    
+    return 0;
+}
--- Algorithms/two_sum.cpp
+++ Algorithms/two_sum.cpp
@@ -4,6 +4,10 @@
   Date: Sep 2nd, 2016
 */
 
+#include <cstdio>
+#include <map>
+#include <vector>
+
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
@@ -22,3 +26,15 @@ public:
         return result;
     }
 };
+
+int main() {
+    int A[] = {2, 7, 11, 15};
+    std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
+
+    Solution s;
+    const int target = 9;
+    const std::vector<int> R = s.twoSum(V, target);
+    printf("[%d] + [%d] == [%d]\n", R[0], R[1], target);
+    
+    return 0;
+}
