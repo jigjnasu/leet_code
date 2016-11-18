9413e34221bb13db831efc76e03df3cb1d62d976
cpp-rakesh <cpp.rakesh@gmail.com>
2016-11-18 19:41:20 +1100

    Find the Difference solution

---
+++ Algorithms/find_the_difference.cpp
@@ -0,0 +1,33 @@
+/*
+  LeetCode.com
+  Solution for Find the Difference
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: Nov 18th, 2016
+ */
+
+#include <cstdio>
+#include <string>
+
+class Solution {
+public:
+    char findTheDifference(std::string s, std::string t) {
+        int loner = 0;
+        std::size_t i = 0;
+
+        while (i < s.size()) {
+            loner += t[i] - s[i];
+            ++i;
+        }
+
+        return loner += t[i];
+    }
+};
+
+int main() {
+    Solution sol;
+    const std::string s = "abcdef";
+    const std::string t = "fdercba";
+
+    printf("[%c] is extra in [%s] compare to [%s]\n", sol.findTheDifference(s, t), t.c_str(), s.c_str());
+    return 0;
+}
