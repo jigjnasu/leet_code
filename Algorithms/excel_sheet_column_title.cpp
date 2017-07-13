6fda146724e5b424b4e9a74c73a2769456e7ee57
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-14 01:24:21 +1000

    adding solution for Excel Sheet Column Title

---
+++ Algorithms/excel_sheet_column_title.cpp
@@ -0,0 +1,29 @@
+/*
+  Solution for Excel Sheet Column Title
+  Author: cpp.rakesh@gmail.com
+  13/07/2017
+*/
+
+#include <cstdio>
+#include <string>
+
+class Solution {
+public:
+    std::string convertToTitle(int n) {
+        std::string s;
+        while (n) {
+            s = static_cast<char>(((n - 1) % 26 + 'A')) + s;
+            n = (n - 1) / 26;
+        }
+
+        return s;
+    }
+};
+
+int main() {
+    int n = 84;
+    Solution s;
+    printf("%s\n", s.convertToTitle(n).c_str());
+
+    return 0;
+}
