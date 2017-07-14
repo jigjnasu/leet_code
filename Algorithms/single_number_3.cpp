3d6da8e2cce20bb238fed2c684a356372884d72f
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-14 20:57:34 +1000

    adding solution for Single Number III

---
+++ Algorithms/single_number_3.cpp
@@ -0,0 +1,37 @@
+/*
+  Solution for Single Number III
+  Author: cpp.rakesh@gmail.com
+  14/07/2017
+*/
+
+#include <cstdio>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> singleNumber(std::vector<int>& nums) {
+        std::vector<int> result(2, 0);
+        int bits = 0;
+        for (std::size_t i = 0; i < nums.size(); ++i)
+            bits ^= nums[i];
+
+        bits &= -bits;
+
+        for (std::size_t i = 0; i < nums.size(); ++i)
+            if (nums[i] & bits)
+                result[0] ^= nums[i];
+            else
+                result[1] ^= nums[i];
+
+        return result;
+    }
+};
+
+int main() {
+    Solution s;
+    std::vector<int> n = {1, 2, 1, 3, 2, 5};
+    const std::vector<int> r = s.singleNumber(n);
+    printf("%d %d\n", r[0], r[1]);
+    
+    return 0;
+}
