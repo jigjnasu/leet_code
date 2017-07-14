4715c844b91c58b40cd0f79b7330df1fc53d7bd9
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-14 21:02:00 +1000

    adding solution for  Single Element in a Sorted Array

---
+++ Algorithms/single_element_in_a_sorted_array.cpp
@@ -0,0 +1,43 @@
+/*
+  Solution for Single Number III
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: 14/07/2017
+*/
+
+#include <cstdio>
+#include <vector>
+
+class Solution {
+public:
+    int singleNonDuplicate(const std::vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (1) {
+            if (start == end) {
+                return nums[start];
+            } else {
+                const int mid = (start + end) >> 1;
+                if (mid % 2) {
+                    if (nums[mid] == nums[mid - 1])
+                        start = mid + 1;
+                    else
+                        end = mid - 1;
+                } else {
+                    if (nums[mid] == nums[mid + 1])
+                        start = mid + 2;
+                    else
+                        end = mid - 2;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    std::vector<int> n = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    printf("%d\n", s.singleNonDuplicate(n));
+
+    return 0;
+}
