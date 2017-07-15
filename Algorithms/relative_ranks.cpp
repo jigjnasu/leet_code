65dad2e61b484e71cb467c72a7ff2d6cd3884ac1
Rakesh Kumar <cpp.rakesh@gmail.com>
2017-07-15 12:19:56 +1000

    adding solution Relative Ranks

--- Algorithms/relative_ranks.cpp
+++ Algorithms/relative_ranks.cpp
@@ -7,45 +7,76 @@
 #include <cstdio>
 #include <vector>
 #include <string>
-#include <map>
-#include <sstream>
+#include <cstdlib>
 
 class Solution {
 public:
     std::vector<std::string> findRelativeRanks(std::vector<int>& nums) {
-        std::vector<std::string> ranks(nums.size(), "");
-        std::map<int, int> hash;
+        std::vector<int> pos;
         for (std::size_t i = 0; i < nums.size(); ++i)
-            hash[nums[i]] = i;
+            pos.push_back(i);
 
-        int i = 0;
-        for (std::map<int, int>::const_iterator it = hash.begin();
-             it != hash.end(); ++it, ++i)
-            ranks[it->second] = m_convert(nums.size() -  i);
+        m_quick_sort(nums, pos, 0, nums.size() - 1);
+
+        const std::size_t size = nums.size();
+        std::vector<std::string> ranks(nums.size(), "");
+        
+        for (std::size_t i = 0; i < nums.size(); ++i) {
+            if (i == 0)
+                ranks[pos[i]] = "Gold Medal";
+            else if (i == 1)
+                ranks[pos[i]] = "Silver Medal";
+            else if (i == 2)
+                ranks[pos[i]] = "Bronze Medal";
+            else
+                ranks[pos[i]] = std::to_string(i + 1);
+        }
+            
         return ranks;
     }
 
 private:
-    std::string m_convert(int n) const {
-        std::string r;
-        if (n == 1) {
-            r = "Gold Medal";
-        } else if (n == 2) {
-            r = "Silver Medal";
-        } else if (n == 3) {
-            r = "Bronze Medal";
-        } else {
-            std::ostringstream oss;
-            oss << n;
-            r = oss.str();
+    void m_quick_sort(std::vector<int>& nums, std::vector<int>& pos, int start, int end) const {
+        if (start < end) {
+            const int mid = m_partition(nums, pos, start, end);
+            m_quick_sort(nums, pos, start, mid - 1);
+            m_quick_sort(nums, pos, mid + 1, end);
+        }
+    }
+
+    int m_partition(std::vector<int>& nums, std::vector<int>& pos, int start, int end) const {
+        const int index = m_random(start, end);
+        m_swap(nums[index], nums[end]);
+        m_swap(pos[index], pos[end]);
+
+        int i = start;
+        for (int j = start; j < end; ++j) {
+            if (nums[j] >= nums[end]) {
+                m_swap(nums[i], nums[j]);
+                m_swap(pos[i], pos[j]);
+                ++i;
+            }
         }
 
-        return r;
+        m_swap(nums[i], nums[end]);
+        m_swap(pos[i], pos[end]);
+
+        return i;
+    }
+
+    int m_random(int min, int max) const {
+        return min + rand() % (max - min + 1);
+    }
+
+    void m_swap(int& a, int& b) const {
+        const int t = a;
+        a = b;
+        b = t;
     }
 };
 
 int main() {
-    std::vector<int> r = {5, 4, 3, 2, 1};
+    std::vector<int> r = {5, 1, 4, 3, 2};
     Solution s;
     const std::vector<std::string> ranks = s.findRelativeRanks(r);
     for (std::size_t i = 0; i < ranks.size(); ++i)
