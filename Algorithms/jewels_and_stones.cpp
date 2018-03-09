48843f0a3410b6ca99489330828b6dacd06d82eb
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-10 09:52:12 +1100

    solution of Jewels and Stones

---
+++ Algorithms/jewels_and_stones.cpp
@@ -0,0 +1,43 @@
+/*
+  LeetCode.com
+  Solution for Jewels and Stones
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: 10/03/2018
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        string j_s(26, 0);
+        string j_c(26, 0);
+        for (size_t i = 0; i < J.size(); ++i)
+            if (J[i] >= 'a' && J[i] <= 'z')
+                j_s[J[i] - 'a'] = 1;
+            else
+                j_c[J[i] - 'A'] = 1;
+
+        int count = 0;
+        for (size_t i = 0; i < S.size(); ++i)
+            if (S[i] >= 'a' && S[i] <= 'z') {
+                if (j_s[S[i] - 'a'])
+                    ++count;
+            } else {
+                if (j_c[S[i] - 'A'])
+                    ++count;
+            }
+        return count;
+    }
+};
+
+int main() {
+    Solution s;
+    string J = "z";
+    string S = "ZZ";
+    printf("%d\n", s.numJewelsInStones(J, S));
+
+    return 0;
+}
