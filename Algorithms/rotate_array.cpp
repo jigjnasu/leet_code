ca4521e870b0a6a08bd9635eec095f2b2cb13cce
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-30 10:30:23 +1100

    Rotate Array Solution

---
+++ Algorithms/rotate_array.cpp
@@ -0,0 +1,30 @@
+/*
+  LeetCode.com
+  Solution for Rotate Array
+  Rakesh Kumar @ cpp.rakesh@gmail.com
+  Date: 30/03/2018
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> v(nums.size(), 0);
+        for (size_t i = 0; i < nums.size(); ++i)
+            v[(i + k) % nums.size()] = nums[i];
+
+        nums = v;
+    }
+};
+
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+    Solution s;
+    s.rotate(v, 3);
+
+    return 0;
+}
