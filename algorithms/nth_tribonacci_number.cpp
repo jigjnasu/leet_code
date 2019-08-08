366fc4c8e71e41a7803c2393d9d2d137cb1a4f18
Rakesh Kumar <cpp.rakesh@gmail.com>
2019-08-08 23:36:58 +0800

    Nth tibonacci number

---
+++ algorithms/nth_tribonacci_number.cpp
@@ -0,0 +1,37 @@
+/*
+  Solution for Nth tribonacci number
+  Author: cpp.rakesh@gmail.com
+  Date: 08/08/2019
+*/
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int i = 3;
+        while (i <= n) {
+            const int t = a + b + c;
+            a = b;
+            b = c;
+            c = t;
+            ++i;
+        }
+
+        return c;
+    }
+};
+
+int main() {
+    Solution s;
+    printf("%d\n", s.tribonacci(4));
+    printf("%d\n", s.tribonacci(25));
+
+    return 0;
+}
