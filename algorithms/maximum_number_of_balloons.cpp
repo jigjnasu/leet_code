6470b83370b431a772d8ddb4749f725ef9035c61
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-04-20 16:32:41 +1000

    maximum number of ballons

---
+++ algorithms/maximum_number_of_balloons.cpp
@@ -0,0 +1,30 @@
+/*
+  LeetCode.com
+  Solution for Maximum number of ballons
+  Rakesh Kumar @ cpp.rakesh(at)gmail.com
+  Date: 20/04/2020
+*/
+
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int maxNumberOfBalloons(std::string text) {
+        int d[26] = {0};
+        for (char c : text)
+            ++d[c - 'a'];
+
+        int a = std::min(std::min(d[0], d[1]), d[13]); // min of a, b and n
+        int b = std::min(d[11], d[14]); // min of l and o
+        return std::min(a, b >> 1);
+    }
+};
+
+int main() {
+    Solution s;
+    printf("%d\n", s.maxNumberOfBalloons("nlaebolko"));
+    printf("%d\n", s.maxNumberOfBalloons("loonbalxballpoon"));
+    printf("%d\n", s.maxNumberOfBalloons("leetcode"));
+
+    return 0;
+}
