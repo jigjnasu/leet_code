7e947ce5be12220e9b0f202bcd480cce1ef050db
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-05-20 02:55:30 +1000

    rename

--- algorithms/268_missing_number.cpp
+++ algorithms/268_missing_number.cpp
@@ -1,5 +1,6 @@
 /*
-  Solution for Missing Number from Leet Code
+  Problem No: 268
+  Problem: Missing Number from Leet Code
   Rakesh Kumar @ cpp.rakesh@gmail.com
   Date: 25th, Nov 2016
  */
@@ -22,23 +23,23 @@ public:
     int missingNumber1(std::vector<int>& nums) {
         int i = 0;
         int result = 0;
-        
+
         while (i < nums.size()) {
             result ^= i ^ nums[i];
             ++i;
         }
 
         return result ^ i;
-    }    
+    }
 };
 
 int main() {
     int A[] = {10, 8, 7, 6, 9, 0, 1, 3, 5, 4};
     std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
-    
+
     Solution s;
     printf("Missing Number == [%d]\n", s.missingNumber(V));
-    printf("Missing Number == [%d]\n", s.missingNumber1(V));    
-    
+    printf("Missing Number == [%d]\n", s.missingNumber1(V));
+
     return 0;
 }
