f1ae07d8bbc5b66df26829ab47d3ccd29a52836a
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-05-21 00:59:08 +1000

    rename

--- algorithms/985_sum_of_even_numbers_after_queries.cpp
+++ algorithms/985_sum_of_even_numbers_after_queries.cpp
@@ -1,6 +1,7 @@
 /*
   LeetCode.com
-  Solution for Sum of even numbers after queries
+  Problem No: 985
+  Problem: Sum of even numbers after queries
   Rakesh Kumar @ cpp.rakesh at gmail.com
   Date: 20/04/2020
 */
