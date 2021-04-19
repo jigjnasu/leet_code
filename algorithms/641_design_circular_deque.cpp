be3b10baea51f2af3b9deb96c5c356205fc377a2
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-04-20 02:15:00 +1000

    641

--- algorithms/641_design_circular_deque.cpp
+++ algorithms/641_design_circular_deque.cpp
@@ -8,10 +8,90 @@
 
 #include <bits/stdc++.h>
 
-class MyCircularDeque {
+/*
+  front pointer at front or 0th index
+  last pointer at last or n - 1th index
+ */
+class MyCircularDeque1 {
 public:
     /** Initialize your data structure here. Set the size of the deque to be k. */
-    MyCircularDeque(int k) : deque(k, 0), fp(k - 1), lp(0), k(k) {}
+    MyCircularDeque1(int k) : deque(k, 0), fp(0), lp(k - 1), k(k) {}
+
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    bool insertFront(int value) {
+        if (size == k)
+            return false;
+        deque[fp] = value;
+        fp = (fp + 1) % k;
+        ++size;
+        return true;
+    }
+
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    bool insertLast(int value) {
+        if (size == k)
+            return false;
+        deque[lp] = value;
+        lp = (lp - 1 + k) % k;
+        ++size;
+        return true;
+    }
+
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    bool deleteFront() {
+        if (size == 0)
+            return false;
+        fp = (fp - 1 + k) % k;
+        --size;
+        return true;
+    }
+
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    bool deleteLast() {
+        if (size == 0)
+            return false;
+        lp = (lp + 1) % k;
+        --size;
+        return true;
+    }
+
+    /** Get the front item from the deque. */
+    int getFront() {
+        if (size == 0)
+            return -1;
+        return deque[(fp - 1 + k) % k];
+    }
+
+    /** Get the last item from the deque. */
+    int getRear() {
+        if (size == 0)
+            return -1;
+        return deque[(lp + 1) % k];
+    }
+
+    /** Checks whether the circular deque is empty or not. */
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular deque is full or not. */
+    bool isFull() {
+        return size == k;
+    }
+
+private:
+    std::vector<int> deque;
+    int fp = 0, lp = 0, size = 0, k = 0;
+};
+
+/*
+  front pointer at last or n - 1th index
+  last pointer at front or 0th index
+ */
+class MyCircularDeque2 {
+public:
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    MyCircularDeque2(int k) : deque(k, 0), fp(k - 1), lp(0), k(k) {}
 
     /** Adds an item at the front of Deque. Return true if the operation is successful. */
     bool insertFront(int value) {
@@ -81,7 +161,7 @@ private:
 };
 
 void test_a() {
-    MyCircularDeque deque(3);
+    MyCircularDeque1 deque(3);
     printf("%d\n", deque.insertLast(1));
     printf("%d\n", deque.insertLast(2));
     printf("%d\n", deque.insertFront(3));
@@ -94,7 +174,7 @@ void test_a() {
 }
 
 void test_b() {
-    MyCircularDeque deque(4);
+    MyCircularDeque1 deque(4);
     printf("%d\n", deque.insertFront(9));
     printf("%d\n", deque.deleteLast());
     printf("%d\n", deque.getRear());
@@ -108,10 +188,39 @@ void test_b() {
     printf("%d\n", deque.insertFront(6));
 }
 
+void test_c() {
+    MyCircularDeque1 deque(3);
+    printf("%d\n", deque.insertLast(1));
+    printf("%d\n", deque.insertLast(2));
+    printf("%d\n", deque.insertFront(3));
+    printf("%d\n", deque.insertLast(4));
+    printf("%d\n", deque.getRear());
+    printf("%d\n", deque.isFull());
+    printf("%d\n", deque.deleteLast());
+    printf("%d\n", deque.insertFront(4));
+    printf("%d\n", deque.getFront());
+}
+
+void test_d() {
+    MyCircularDeque1 deque(4);
+    printf("%d\n", deque.insertFront(9));
+    printf("%d\n", deque.deleteLast());
+    printf("%d\n", deque.getRear());
+    printf("%d\n", deque.getFront());
+    printf("%d\n", deque.getFront());
+    printf("%d\n", deque.deleteFront());
+    printf("%d\n", deque.insertFront(6));
+    printf("%d\n", deque.insertLast(5));
+    printf("%d\n", deque.insertFront(9));
+    printf("%d\n", deque.getFront());
+    printf("%d\n", deque.insertFront(6));
+}
 
 int main() {
     test_a();
     test_b();
+    test_c();
+    test_d();
 
     return 0;
 }
