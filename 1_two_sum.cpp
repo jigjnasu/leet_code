76de10eb0b3f66f85b103f75c37db1cb327c70ff
Rakesh Kumar <cpp.rakesh@gmail.com>
2021-05-17 03:00:13 +1000

    optimized code

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -6,27 +6,24 @@
   Date: Sep 2nd, 2016
 */
 
-#include <cstdio>
-#include <map>
-#include <vector>
+#include <bits/stdc++.h>
 
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::vector<int> result;
-        std::map<int, int> dictionary;
+        std::vector<int> r(2, 0);
+        std::unordered_map<int, int> dict;
         for (std::size_t i = 0; i < nums.size(); ++i) {
-            const int diff = target - nums[i];
-            std::map<int, int>::const_iterator it = dictionary.find(diff);
-            if (it != dictionary.end()) {
-                result.push_back(it->second);
-                result.push_back(i);
-                return result;
+            const int d = target - nums[i];
+            if (dict.find(d) != dict.end()) {
+                r[0] = dict[d];
+                r[1] = i;
+                break;
             }
-            dictionary.insert(std::pair<int, int>(nums[i], i));
+            dict[nums[i]] = i;
         }
-        return result;
-    }
+        return r;
+   }
 };
 
 int main() {
