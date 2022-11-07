0480df7e85a5b25a9f01615ef1044f5522292ae7
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-11-07 17:13:03 +1100

    utility linked list code

--- 237_delete_node_in_a_linked_list.cpp
+++ 237_delete_node_in_a_linked_list.cpp
@@ -6,74 +6,29 @@
   Date: 07/05/2021
  */
 
+#include "utility/SingleLinkedList.h"
 #include <bits/stdc++.h>
-
-struct ListNode {
-    ListNode() {}
-    ListNode(int v) : val(v) {}
-
-    int val = 0;
-    ListNode* next = nullptr;
-};
 
 class Solution {
 public:
-    ~Solution() {
-        ListNode* node = head;
-        while (node) {
-            ListNode* temp = node;
-            node = node->next;
-            delete temp;
-        }
-    }
-
-    void push_back(int val) {
-        if (head == nullptr)
-            head = new ListNode(val);
-        else {
-            ListNode* node = head;
-            while (node->next)
-                node = node->next;
-            node->next = new ListNode(val);
-        }
-    }
-
-    void traverse() {
-        printf("---------------------------------------------\n");
-        ListNode* node = head;
-        while (node) {
-            printf("%d ", node->val);
-            node = node->next;
-        }
-        printf("\n---------------------------------------------\n");
-    }
-
-    ListNode* get(int index) {
-        ListNode* node = head;
-        for (int i = 0; i < index; ++i)
-            node = node->next;
-        return node;
-    }
-
     void deleteNode(ListNode* node) {
         ListNode* next = node->next;
         *node = *next;
         delete next;
     }
-
-private:
-    ListNode* head = nullptr;
 };
 
 int main() {
+    SingleLinkedList ls;
     Solution s;
+    ListNode* head = nullptr;
     for (int i = 1; i <= 10; ++i)
-        s.push_back(i);
-    s.traverse();
+        head = ls.push_back(head, i);
+    ls.traverse(head);
 
-    ListNode* node = s.get(4);
+    ListNode* node = head->next->next->next;
     s.deleteNode(node);
-    s.traverse();
+    ls.traverse(head);
 
     return 0;
 }
