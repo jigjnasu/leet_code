ac644a0b1652893c413cdda47c45a6ac22a3505b
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-11-08 01:13:18 +1100

    234 palindrome linked list

--- 234_palindrome_linked_list.cpp
+++ 234_palindrome_linked_list.cpp
@@ -12,6 +12,28 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+        ListNode *prev = nullptr, *curr = head, *fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        if (fast)
+            curr = curr->next;
+        
+        while (prev && curr) {
+            if (prev->val != curr->val)
+                return false;
+            prev = prev->next;
+            curr = curr->next;
+        }
+        return true;
+    }
+    
+    bool isPalindrome_1(ListNode* head) {
         std::string str;
         while (head) {
             str.push_back(head->val + '0');
@@ -35,11 +57,24 @@ void test_case_1() {
     ls.traverse(head);
 
     Solution s;
-    printf("%d\n", s.isPalindrome(head));
+    printf("%d\n", s.isPalindrome_1(head));
+}
+
+void test_case_2() {
+    SingleLinkedList ls;
+    ListNode* head = nullptr;
+    for (int i = 1; i <= 9; ++i)
+        head = ls.push_back(head, i);
+    ls.traverse(head);
+
+    Solution s;
+    head = s.isPalindrome(head);
+    ls.traverse(head);
 }
 
 int main() {
-    test_case_1();
+    //test_case_1();
+    test_case_2();
     
     return 0;
 }
