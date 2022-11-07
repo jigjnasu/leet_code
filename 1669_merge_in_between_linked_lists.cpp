7123ee92d5f993029d6d1e7b378db067e7977b34
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-11-07 16:59:56 +1100

    single linked list utility code

--- 1669_merge_in_between_linked_lists.cpp
+++ 1669_merge_in_between_linked_lists.cpp
@@ -6,25 +6,11 @@
   Date: 30/04/2021
 */
 
+#include "utility/SingleLinkedList.h"
 #include <bits/stdc++.h>
-
-struct ListNode {
-    ListNode() {}
-    ListNode(int v) : val(v) {}
-
-    int val = 0;
-    ListNode* next = nullptr;
-};
 
 class Solution {
 public:
-    void push_back(ListNode* list, int val) {
-        ListNode* node = list;
-        while (node->next != nullptr)
-            node = node->next;
-        node->next = new ListNode(val);
-    }
-
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
         ListNode* n1 = list1;
         for (int i = 0; i < a - 1; ++i)
@@ -41,32 +27,23 @@ public:
         n1->next = list2;
         return list1;
     }
-
-    void traverse(ListNode* list) const {
-        printf("-----------------------------------------------------\n");
-        ListNode* node = list;
-        while (node) {
-            printf("%d ", node->val);
-            node = node->next;
-        }
-        printf("\n-----------------------------------------------------\n");
-    }
 };
 
 int main() {
     Solution s;
+    SingleLinkedList ls;
     ListNode* list1 = new ListNode(0);
     for (int i = 1; i <= 10; ++i)
-        s.push_back(list1, i);
-    s.traverse(list1);
+        list1 = ls.push_back(list1, i);
+    ls.traverse(list1);
 
     ListNode* list2 = new ListNode(100);
     for (int i = 101; i <= 110; ++i)
-        s.push_back(list2, i);
-    s.traverse(list2);
+        list2 = ls.push_back(list2, i);
+    ls.traverse(list2);
 
     list1 = s.mergeInBetween(list1, 3, 7, list2);
-    s.traverse(list1);
+    ls.traverse(list1);
 
     return 0;
 }
