9f98d8610dde20d90c3924391e242c57a8b98c98
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-11-07 17:06:44 +1100

    single linked list utility code

--- 21_merge_two_sorted_lists.cpp
+++ 21_merge_two_sorted_lists.cpp
@@ -6,50 +6,9 @@
   Date: 07/11/2022
 */
 
+#include "utility/common.h"
+#include "utility/SingleLinkedList.h"
 #include <bits/stdc++.h>
-
-struct ListNode {
-    ListNode(int v) : val(v), next(nullptr) {}
-    ListNode(int v, ListNode* n) : val(v), next(n) {}
-
-    int val = 0;
-    ListNode* next = nullptr;
-};
-
-ListNode* push_back(ListNode* head, int val) {
-    if (head == nullptr) {
-        head = new ListNode(val);
-    } else {
-        ListNode* node = head;
-        while (node->next)
-            node = node->next;
-        node->next = new ListNode(val);
-    }
-    return head;
-}
-
-void traverse(ListNode* head) {
-    ListNode* node = head;
-    while (node) {
-        printf("%d ", node->val);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-int random(int s, int e) {
-    std::random_device rd;
-    std::uniform_int_distribution<> dt(s, e);
-    return dt(rd);
-}
-
-std::vector<int> get_sorted_array(int n) {
-    std::vector<int> v(n, 0);
-    for (std::size_t i = 0; i < v.size(); ++i)
-        v[i] = random(1, 100);
-    std::sort(v.begin(), v.end());
-    return v;
-}
 
 class Solution {
 public:
@@ -78,31 +37,33 @@ public:
 };
 
 void test_case_1() {
+    SingleLinkedList ls;
     ListNode* head = nullptr;
     for (int i = 1; i <= 20; ++i)
-        head = push_back(head, i);
-    traverse(head);
+        head = ls.push_back(head, i);
+    ls.traverse(head);
 }
 
 void test_case_2() {
-    std::vector<int> v = get_sorted_array(10);
+    SingleLinkedList ls;
+    std::vector<int> v = get_sorted_array(10, 1, 100);
     ListNode* head1 = nullptr;
     for (int& e : v)
-        head1 = push_back(head1, e);
-    v = get_sorted_array(25);
+        head1 = ls.push_back(head1, e);
+    v = get_sorted_array(25, 1, 100);
     ListNode* head2 = nullptr;
     for (int& e : v)
-        head2 = push_back(head2, e);
-    traverse(head1);
-    traverse(head2);
+        head2 = ls.push_back(head2, e);
+    ls.traverse(head1);
+    ls.traverse(head2);
 
     Solution s;
     ListNode* head = s.mergeTwoLists(head1, head2);
-    traverse(head);
+    ls.traverse(head);
 }
 
 int main() {
-    //test_case_1();
+    test_case_1();
     test_case_2();
 
     return 0;
